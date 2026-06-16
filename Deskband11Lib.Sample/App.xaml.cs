8e7fd1be97b9ae64629cda77b44e41f18d9746c1
Howon Lee <kck4156@kagamine-rin.com>
2026-06-16 16:53:51 +0900

    Removed default comment + refactor

--- Deskband11Lib.Sample/App.xaml.cs
+++ Deskband11Lib.Sample/App.xaml.cs
@@ -1,30 +1,13 @@
 ﻿using Microsoft.UI.Xaml;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace Deskband11Lib.Sample;
 
-/// <summary>
-/// Provides application-specific behavior to supplement the default Application class.
-/// </summary>
 public partial class App : Application
 {
     private MainWindow? _window;
 
-    /// <summary>
-    /// Initializes the singleton application object.  This is the first line of authored code
-    /// executed, and as such is the logical equivalent of main() or WinMain().
-    /// </summary>
-    public App()
-    {
-        InitializeComponent();
-    }
+    public App() => InitializeComponent();
 
-    /// <summary>
-    /// Invoked when the application is launched.
-    /// </summary>
-    /// <param name="args">Details about the launch request and process.</param>
     protected override async void OnLaunched(LaunchActivatedEventArgs args) => await InitializeMainWindow();
 
     private async Task InitializeMainWindow()
--- Deskband11Lib.Sample/MainWindow.xaml.cs
+++ Deskband11Lib.Sample/MainWindow.xaml.cs
@@ -4,11 +4,6 @@ using Microsoft.UI.Xaml;
 
 namespace Deskband11Lib.Sample;
 
-/// <summary>
-/// The application window. This hosts a Frame that displays pages. Add your
-/// UI and logic to MainPage.xaml / MainPage.xaml.cs instead of here so you
-/// can use Page features such as navigation events and the Loaded lifecycle.
-/// </summary>
 public sealed partial class MainWindow : Window
 {
     public TaskbarContentHost TaskbarContentHost { get; }
